add253911b4c7aaa44177e2b8e850689c3fc3b96
Charlie Nguyen <charliekid19@gmail.com>
2020-09-28 14:39:43 -0700

    got timer to work

---
+++ Assets/Scripts/LevelStats.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelStats : MonoBehaviour
+{
+    [SerializeField] private Text scoreText;
+
+    [SerializeField] private Text levelText;
+
+    [SerializeField] private Text  timeText;
+
+    private float initialTime = 400;
+
+    private float currentTime = 400;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentTime > 0)
+        {
+            currentTime -= Time.deltaTime;
+            timeText.text = Math.Floor(currentTime) + ""; 
+        }
+        
+    }
+}
